89166c47a857789053bf120ae947d5a57c5e2bc5
Java Poo - Aula2 <ciceroleandro@gmail.com>
2023-03-14 19:53:39 -0300

    poo java - aula02

---
+++ src/Caneta.java
@@ -0,0 +1,33 @@
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    public void status(){
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta " + this.ponta);
+        System.out.println("Tampada " + this.tampada);
+        System.out.println("carga " + this.carga);
+    }
+
+
+    public void rabiscar(){
+        if(this.tampada==true){
+            System.out.println("ERRO! Não posso rabiscar");
+        }else{
+            System.out.println("Estou rabiscando");
+        }
+
+
+    }
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){    
+        this.tampada = false;
+    }
+
+}
